9c445e786474b3ccb122a5c3e6fa615ddc94504d
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2022-01-26 19:40:01 -0700

    Initial commit

---
+++ Mission4Assignment_ZM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Mission4Assignment_ZM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
